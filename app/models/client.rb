29b40a93be32f6221e91f9d09a713ceaf0cf1431
Tom Lee <tom_t_lee@yahoo.com>
2015-11-26 14:36:11 -0500

    Add validation for unique client names

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,5 @@
 class Client < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates_uniqueness_of :first_name, scope: :last_name
 end
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -3,4 +3,5 @@ require "spec_helper"
 describe Client do
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
+  it { should validate_uniqueness_of(:first_name).scoped_to(:last_name) }
 end
