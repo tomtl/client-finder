2610c35c91ab5d1b9a633e9f7903b071c9786d00
Tom Lee <tom_t_lee@yahoo.com>
2015-11-26 14:30:56 -0500

    Refactor sights controller spec

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -10,42 +10,38 @@ describe ClientsController do
 
   describe "POST create" do
     context "with valid input" do
+      let(:client1) { Fabricate.attributes_for(:client) }
+      before { post :create, client: client1 }
+
       it "creates the client record" do
-        client1 = Fabricate.attributes_for(:client)
-        post :create, client: client1
         expect(Client.first.first_name).to eq(client1[:first_name])
       end
 
       it "sets the success message" do
-        client1 = Fabricate.attributes_for(:client)
-        post :create, client: client1
         expect(flash[:success]).to be_present
       end
 
       it "redirects to the client page" do
-        post :create, client: {first_name: "Andy", last_name: "Anderson"}
         expect(response).to redirect_to(client_path(Client.first.id))
       end
     end
 
     context "with invalid input" do
+      before { post :create, client: { first_name: "Bob" } }
+
       it "does not create the record" do
-        post :create, client: { first_name: "Bob" }
         expect(Client.count).to eq(0)
       end
 
       it "sets @client" do
-        post :create, client: { first_name: "Bob" }
         expect(assigns(:client)).to be_instance_of(Client)
       end
 
       it "sets the error message" do
-        post :create, client: { first_name: "Bob" }
         expect(flash[:danger]).to be_present
       end
-      
+
       it "renders the :create template" do
-        post :create, client: { first_name: "Bob" }
         expect(response).to render_template :new
       end
     end
