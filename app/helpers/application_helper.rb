02ecd5bb2678020f7243bdfc7d802c05c0606d8b
Tom <tom_t_lee@yahoo.com>
2015-11-30 20:34:07 +0000

    Add client#show map image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def single_location_map_link(address)
+    "https://maps.google.com/maps/api/staticmap?size=400x300&
+    sensor=false&zoom=12&markers=#{address}"
+  end
 end
